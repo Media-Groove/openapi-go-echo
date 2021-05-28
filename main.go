421c88500a006dbb14aa1c0d5c075cea648ddf56
Your Name <you@example.com>
2021-05-28 14:23:26 +0900

    feat: サンプルEchoサーバ設定

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"github.com/Media-Groove/openapi-go-echo/openapi"
+	"github.com/Media-Groove/openapi-go-echo/sampleapi"
+	"github.com/Media-Groove/openapi-go-echo/server"
+	"github.com/labstack/echo/v4"
+	"time"
+)
+
+func main() {
+	server.Start(&Initializer{}, 500*time.Millisecond)
+}
+
+type Initializer struct{}
+
+func (i Initializer) Initialize(_ context.Context) {
+}
+
+func (i Initializer) EchoSetup(e *echo.Echo) {
+	openapi.SetRoutes(e, &openapi.NoMiddlewares{}, sampleapi.NewSampleApiController(sampleapi.NewSampleApiService()))
+}
